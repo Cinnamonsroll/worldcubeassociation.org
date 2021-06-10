98fb55a21dff499d27d6ec8cdd608e186dc8f829
Cinnamonsroll <71993095+Cinnamonsroll@users.noreply.github.com>
2021-06-10 18:43:47 -0400

    Update char-counter.js

--- WcaOnRails/app/assets/javascripts/char-counter.js
+++ WcaOnRails/app/assets/javascripts/char-counter.js
@@ -1,13 +1,13 @@
 $(function() {
-  $(".char-counter").each(function() {
-    var maxLength = $(this).attr("maxlength");
-    if (maxLength !== null) {
-      var commentLength = document.createElement("p");
-      commentLength.style.float = "right";
-      $(this).parent("div").append(commentLength);
-      $(this).on("input", function() {
-        commentLength.innerText = this.value.length + "/" + maxLength;
-      }).trigger("input");
-    }
-  });
+    $(".char-counter").each(function() {
+            let maxLength = $(this).attr("maxlength"),
+                commentLength = document.createElement("p");
+            if (!maxLength) return
+            commentLength.style.float = "right";
+            $(this).parent("div").append(commentLength);
+            $(this).on("input", function() {
+                commentLength.innerText = this.value.length + "/" + maxLength;
+            }).trigger("input");
+        }
+    });
 });
